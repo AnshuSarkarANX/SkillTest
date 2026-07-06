983d29a39ef6aae64605f659fc00f696fc284d93
AnshuSarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2026-07-06 14:37:01 +0530

    feat: add bottomBar, topBar, and padding state management to store

--- src/state/store.js
+++ src/state/store.js
@@ -7,3 +7,29 @@ export const useInterviewDetails = create((set) => ({
   setFile: (newFile) => set({ file: newFile }),
   updateJobDetails: (newJobDetails) => set({ jobDetails: newJobDetails }),
 }));
+
+const bottomBar = create((set) => ({
+  isActive: true,
+  setActive: (isActive) => set({ isActive }),
+}));
+const topBar = create((set) => ({
+  hasBackButton: false,
+  setHasBackButton: (hasBackButton) => set({ hasBackButton }),
+
+  logo: true,
+  setLogo: (logo) => set({ logo }),
+
+  home: false,
+  setHome: (home) => set({ home }),
+
+  onBack: null, // function reference initially null
+  setOnBack: (fn) => {
+    set({ onBack: fn });
+  },
+}));
+const padding = create((set) => ({
+  isActive: true,
+  setActive: (isActive) => set({ isActive }),
+}));
+
+export { bottomBar, topBar, padding };
